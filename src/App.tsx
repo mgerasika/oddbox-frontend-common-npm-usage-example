6750a17be39be449043f2e5a3dbb421395d82be6
Mykhaylo Gerasika <mgerasika@gmail.com>
2022-12-06 19:17:14 +0200

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 
-//!!!Warning wrong!!! 
-// import { Avatar, Button } from 'oddbox-frontend-common'
+//!!!Warning wrong!!!
+// import { Avatar, Button } from 'oddbox-frontend-common' - problem with tree-shaking - bundle over 1-2mbs
 
-// @ts-ignore
-import { Avatar } from 'oddbox-frontend-common/dist/es/general-ui/avatar/avatar.component'
-// @ts-ignore
-import {  Button } from 'oddbox-frontend-common/dist/es/general-ui/button/button.component'
+// !!!Right!!!
+import { Avatar } from "oddbox-frontend-common/dist/general-ui/avatar/avatar.component";
+import { Button } from "oddbox-frontend-common/dist/general-ui/button/button.component";
+import { SocialButtons } from "oddbox-frontend-common/dist/general-ui/social/social.component";
 
 function App() {
-	return (
-		<div >
-			<Button name='hello' variant='orange'>Orange</Button>
-			<Avatar firstName='John' lastName='Dest'></Avatar>
-		</div>
-	);
+  return (
+    <div>
+      <Button name="hello" variant="orange">
+        Orange
+      </Button>
+		  <Avatar firstName="John" lastName="Dest"></Avatar>
+		  <SocialButtons authentication={'x' as any} showFacebook showGoogle/>
+    </div>
+  );
 }
 
 export default App;
